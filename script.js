632119cc20465f811a0e0b96d4db4b8d41bfca08
nikan <nikanemami7@gmail.com>
2024-06-01 06:21:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// step 1
 function getAverage(scores) {
+    let total = 0;
+    for (let i = 0; i < scores.length; i++){
+      total += scores[i];
+    }
+    let average = total / scores.length;
+    return average;
+    }
+    
+    console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+    console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+    console.log(getAverage([1,2,3,4,5,6,7,8,9,10]))
 
-}
-
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+// step 2
